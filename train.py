b419f8623712375d615db6863082fe2aa74ccbe0
Kwanghee Choi <juice500ml@gmail.com>
2017-11-28 00:59:11 +0900

    Half the model, same loss! (weights included)

--- train.py
+++ train.py
@@ -21,16 +21,6 @@ model.add(Conv2D(32, (3,3), activation='relu', strides=(1,1), padding="same", in
 model.add(Conv2D(32, (3,3), activation='relu', strides=(1,1), padding="same", input_shape=(16, 16, 1)))
 model.add(MaxPool2D(pool_size=(2,2), strides=(2,2), padding='same'))
 
-model.add(Conv2D(64, (3,3), activation='relu', strides=(1,1), padding="same", input_shape=(16, 16, 1)))
-model.add(Conv2D(64, (3,3), activation='relu', strides=(1,1), padding="same", input_shape=(16, 16, 1)))
-model.add(Conv2D(64, (3,3), activation='relu', strides=(1,1), padding="same", input_shape=(16, 16, 1)))
-model.add(MaxPool2D(pool_size=(2,2), strides=(2,2), padding='same'))
-
-model.add(Conv2D(128, (3,3), activation='relu', strides=(1,1), padding="same", input_shape=(16, 16, 1)))
-model.add(Conv2D(128, (3,3), activation='relu', strides=(1,1), padding="same", input_shape=(16, 16, 1)))
-model.add(Conv2D(128, (3,3), activation='relu', strides=(1,1), padding="same", input_shape=(16, 16, 1)))
-model.add(MaxPool2D(pool_size=(2,2), strides=(2,2), padding='same'))
-
 model.add(Flatten())
 model.add(Dense(800, activation='relu'))
 model.add(Dense(16*16, activation='softmax'))
@@ -45,4 +35,4 @@ y.resize((y.shape[0], 16*16))
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 checkpoint = ModelCheckpoint(filepath='gomoku.hdf5', verbose=1, save_best_only=True)
-model.fit(x_train, y_train, validation_data=(x_test, y_test), batch_size=512, epochs=30, callbacks=[checkpoint])
+model.fit(x_train, y_train, validation_data=(x_test, y_test), verbose=2, batch_size=512, epochs=20, callbacks=[checkpoint])
